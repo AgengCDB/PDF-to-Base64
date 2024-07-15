03c4ba2a42a59b8391a295a4224c714ce74ed512
AgengCDB <ageng.widjaya7@gmail.com>
2024-07-15 16:52:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 os.system("title PDF to Base64 Text File")
 
 import random
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QVBoxLayout, QPushButton, QTextEdit, QAction, QMessageBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QVBoxLayout, QPushButton, QTextEdit, QAction, QMessageBox, QProgressBar, QStyleFactory
 from PyQt5.QtCore import Qt, QUrl, QThread, pyqtSignal, QObject
 from PyQt5.QtGui import QIcon, QPixmap, QImage
 # from PyQt5.QtCore import QDesktopServices
@@ -23,36 +23,10 @@ colors_light = [
 ]
 random_color = random.choice(colors_light)
 
-def pdf_to_blob(file_paths):
-    output_file_paths = []
-    file_count = 1
-    for file_path in file_paths:
-        try:
-            # Read the PDF file in binary mode
-            with open(file_path, 'rb') as pdf_file:
-                binary_data = pdf_file.read()
-            
-            # Encode the binary data to base64
-            blob_data = base64.b64encode(binary_data).decode('utf-8')
-            
-            # Save base64 data to a text file in the same folder
-            file_name = os.path.basename(file_path)
-            output_file_name = f'base64_{file_name}.txt'
-            output_file_path = os.path.join(os.path.dirname(file_path), output_file_name)
-            
-            with open(output_file_path, 'w') as txt_file:
-                txt_file.write(blob_data)
-            
-            output_file_paths.append(output_file_path)
-            # print(f"Converted {file_path} to base64 and saved to {output_file_path}")
-            
-            print(f"\n=== Output File {file_count}\n{output_file_path}\n")
-            file_count += 1
-        
-        except Exception as e:
-            print(f"Error converting {file_path}: {str(e)}")
-    
-    return output_file_paths
+def decode_base64_to_pdf(base64_data, output_path):
+    binary_data = base64.b64decode(base64_data)
+    with open(output_path, 'wb') as pdf_file:
+        pdf_file.write(binary_data)
 
 class StreamHandler(QObject):
     newText = pyqtSignal(str)
@@ -118,6 +92,13 @@ class MainWindow(QMainWindow):
         self.process_button.clicked.connect(self.process_files)
         self.central_layout.addWidget(self.process_button, alignment=Qt.AlignCenter)
         
+        self.prg = QProgressBar()
+        self.prg.setStyle(QStyleFactory.create("Windows"))
+        self.prg.setVisible(False)
+        self.prg.setAlignment(Qt.AlignCenter)  # Align text to center
+        self.prg.setFormat('%p%')  # Show percentage text
+        self.central_layout.addWidget(self.prg)
+        
         # Add a button
         self.open_button1 = QPushButton("Open Result File")
         self.open_button1.clicked.connect(self.open_result_file)
@@ -175,13 +156,47 @@ class MainWindow(QMainWindow):
         # self.worker.finished.connect(self.worker_finished)
         self.worker.update_result_files.connect(self.result_files_update)
         self.worker.update_file_paths_label.connect(self.file_paths_label_update)
+        
+        self.worker.update_prg.connect(self.prg_update)
+        self.worker.update_prg_maximum.connect(self.prg_update_maximum)
+        self.worker.update_prg_minimum.connect(self.prg_update_minimum)
+        self.worker.update_prg_value.connect(self.prg_update_value)
+        
+        self.worker.update_process_button.connect(self.process_button_update)
+        self.worker.update_open_button1.connect(self.open_button1_update)
+        self.worker.update_open_button.connect(self.open_button_update)
+    
+    def process_button_update(self, setEnabled: bool):
+        self.process_button.setEnabled(setEnabled)
+    
+    def open_button1_update(self, setEnabled: bool):
+        self.open_button1.setEnabled(setEnabled)
+    
+    def open_button_update(self, setEnabled: bool):
+        self.open_button.setEnabled(setEnabled)
     
     def result_files_update(self, x):
         self.result_files = x
     
     def file_paths_label_update(self, x):
         self.label2 = x
-        pass
+    
+    def prg_update(self, x):
+        # print(x)
+        self.prg.setVisible(x)
+        
+    # start progress bar
+    
+    def prg_update_maximum(self, x):
+        self.prg.setMaximum(x)
+    
+    def prg_update_minimum(self, x):
+        self.prg.setMinimum(x)
+    
+    def prg_update_value(self, x):
+        self.prg.setValue(x)
+    
+    # end progress bar
     
     def open_result_file(self):
         if self.result_files:
@@ -215,19 +230,104 @@ class MainWindow(QMainWindow):
 class WorkerThread(QThread):
     update_result_files = pyqtSignal(list)
     update_file_paths_label = pyqtSignal(str)
+    update_prg = pyqtSignal(bool)
+    update_prg_maximum = pyqtSignal(int)
+    update_prg_minimum = pyqtSignal(int)
+    update_prg_value = pyqtSignal(int)
+    
+    update_process_button = pyqtSignal(bool)
+    update_open_button1 = pyqtSignal(bool)
+    update_open_button = pyqtSignal(bool)
     
     def __init__(self, dropped_files):
         super().__init__()
         self.dropped_files = dropped_files
     
+    def pdf_to_blob(self, file_paths):
+        
+        self.update_prg_maximum.emit(0)
+        self.update_prg_maximum.emit(len(file_paths))
+        
+        progress_value = 0
+        self.update_prg_value.emit(progress_value)
+        
+        output_file_paths = []
+        file_count = 1
+        for file_path in file_paths:
+            try:
+                if file_path.lower().endswith('.txt'):
+                    # Read the text file containing base64 data
+                    with open(file_path, 'r') as txt_file:
+                        base64_data = txt_file.read()
+                    
+                    binary_data = base64.b64decode(base64_data)
+                    
+                    file_name = os.path.basename(file_path)
+                    output_file_name = f'base64_{file_name}.pdf'
+                    output_file_path = os.path.join(os.path.dirname(file_path), output_file_name)
+                    
+                    with open(output_file_path, 'wb') as pdf_file:
+                        pdf_file.write(binary_data)
+                    
+                    output_file_paths.append(output_file_path)
+                    
+                    print(f"\n=== Output File {file_count}\n{output_file_path}\n")
+                    file_count += 1
+
+                else:
+                    # Read the PDF file in binary mode
+                    with open(file_path, 'rb') as pdf_file:
+                        binary_data = pdf_file.read()
+                    
+                    # Encode the binary data to base64
+                    blob_data = base64.b64encode(binary_data).decode('utf-8')
+                    
+                    # Save base64 data to a text file in the same folder
+                    file_name = os.path.basename(file_path)
+                    output_file_name = f'base64_{file_name}.txt'
+                    output_file_path = os.path.join(os.path.dirname(file_path), output_file_name)
+                    
+                    with open(output_file_path, 'w') as txt_file:
+                        txt_file.write(blob_data)
+                    
+                    output_file_paths.append(output_file_path)
+                    # print(f"Converted {file_path} to base64 and saved to {output_file_path}")
+                    
+                    print(f"\n=== Output File {file_count}\n{output_file_path}\n")
+                    file_count += 1
+                
+                # end if else
+            
+            except Exception as e:
+                print(f"Error converting {file_path}: {str(e)}")
+            
+            progress_value += 1
+            self.update_prg_value.emit(progress_value)
+            # time.sleep(1)
+        
+        return output_file_paths
+    
     def run(self):
+        self.update_process_button.emit(False)
+        self.update_open_button1.emit(False)
+        self.update_open_button.emit(False)
+        
         if self.dropped_files:
+            self.update_prg.emit(True)
+            # for x in range(1, 10):
+            #     time.sleep(1)
+            #     print(x)
             print("Processing files...")
-            self.update_result_files.emit(pdf_to_blob(self.dropped_files))
+            self.update_result_files.emit(self.pdf_to_blob(self.dropped_files))
             # self.result_files = pdf_to_blob(self.dropped_files)
+            self.update_prg.emit(False)
         else:
             print("There's no file to process")
             self.update_file_paths_label.emit("Drop the file(s) first!")
+        
+        self.update_process_button.emit(True)
+        self.update_open_button1.emit(True)
+        self.update_open_button.emit(True)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
